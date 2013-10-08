3a857c856d114ff139454972fb027c0837e573e0
Jeff Conrad <jeff@sunshinefueledproductions.com>
2013-10-07 22:58:05 -0400

    Fixed most failing tests.
    RandoriBundleProjectTest had a few invalid paths.
    EchoBehaviorTest and VerticalTabsTest needed to be updated with to take into account some refactoring of the Randori framework.
    
    build.gradle was updated to use an older version of Google Guava because Falcon uses a method that was de-public-ed.  Technically Falcon uses Guava r08, but the r08 guava on the maven repository is not a valid jar, so this guava is r09

--- src/test/java/randori/compiler/internal/projects/RandoriBundleProjectTest.java
+++ src/test/java/randori/compiler/internal/projects/RandoriBundleProjectTest.java
@@ -1,19 +1,19 @@
 /***
  * Copyright 2013 Teoti Graphix, LLC.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- * 
- * 
+ *
+ *
  * @author Michael Schmalle <mschmalle@teotigraphix.com>
  */
 
@@ -169,7 +169,11 @@ public class RandoriBundleProjectTest extends RandoriTestCaseBase
 
         File build = new File(TestConstants.RandoriASFramework
                 + "/randori-compiler/temp/build");
-        FileUtils.deleteDirectory(build);
+
+        FileUtils.deleteQuietly(build);
+
+        // TODO: find out why this fails intermittently
+        //FileUtils.deleteDirectory(build);
     }
 
     @Test
@@ -318,7 +322,7 @@ public class RandoriBundleProjectTest extends RandoriTestCaseBase
 
         configuration.setSDKPath(getTestDataPath() + "/sdk/randori-sdk.rbl");
         configuration.addExternalBundlePath(hmssProjectPath
-                + "/LabModule/libs/CommonModule.rbl");
+                + "1/LabModule/libs/CommonModule.rbl");
 
         project.configure(configuration);
         boolean success = project.compile(true, true);
@@ -364,7 +368,7 @@ public class RandoriBundleProjectTest extends RandoriTestCaseBase
         arguments.setJsOutputAsFiles(true);
 
         //arguments.addBundlePath(hmssProjectPath + "/libs/CommonModule.rbl");
-        arguments.addBundlePath(hmssProjectPath + "/libs/LabModule.rbl");
+        arguments.addBundlePath(hmssProjectPath + "2/libs/LabModule.rbl");
 
         arguments.setSDKPath(getTestDataPath() + "/sdk/randori-sdk.rbl");
 
